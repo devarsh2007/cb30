19dc3fc4d0ce7626c22ba64e1713a4da2e8ce9c1
devarsh2007 <sunilpatel2131@gmail.com>
2024-04-12 19:47:40 +0530

    code

---
+++ ifelse1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+      // write a program that check number is positive or negetive.
+      // < , >
+      int a;
+
+      printf("enter number : ");
+      scanf("%d",&a);
+
+      if(0<a)
+      {
+            printf("number is positive");
+      }
+      else
+      {
+           printf("number is negetive"); 
+      }
+
+      printf("\nend of program...");
+      
+}
